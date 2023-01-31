f6599b760816e523cae2adb112713340562a2717
salomaestro <chris10an.salomonsen@gmail.com>
2023-01-31 20:01:18 +0100

    working on a binary search tree approach

---
+++ include/bst.h
@@ -0,0 +1,24 @@
+#include "common.h"
+
+struct tree;
+typedef struct tree tree_t;
+
+struct tree_iter;
+typedef struct tree_iter tree_iter_t;
+
+tree_t *tree_create(cmpfunc_t cmpfunc);
+
+void tree_delete(tree_t *tree);
+
+size_t tree_size(tree_t *tree);
+
+int tree_add(tree_t *tree, void *elem);
+
+
+tree_iter_t *tree_createiter(tree_t *tree);
+
+void tree_destroyiter(tree_iter_t *iter);
+
+int tree_hasnext(tree_iter_t *iter);
+
+void *tree_next(tree_iter_t *iter);
