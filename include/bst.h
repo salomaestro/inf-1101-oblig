aeb91506e2e62112db9fe38174126215c4234825
salomaestro <chris10an.salomonsen@gmail.com>
2023-02-02 01:10:27 +0100

    rename tree_delete -> tree_destroy new function tree_find

--- include/bst.h
+++ include/bst.h
@@ -8,12 +8,15 @@ typedef struct tree_iter tree_iter_t;
 
 tree_t *tree_create(cmpfunc_t cmpfunc);
 
-void tree_delete(tree_t *tree);
+void tree_destroy(tree_t *tree);
 
 size_t tree_size(tree_t *tree);
 
 int tree_add(tree_t *tree, void *elem);
 
+int tree_find(tree_t *tree, void *elem);
+
+// int tree_maxdepth(tree_t *tree);
 
 tree_iter_t *tree_createiter(tree_t *tree);
 
